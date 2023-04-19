42576b522367d0e3d306a08522c498f97e97a463
Maygol Kananizadeh <mkananizadeh@confluent.io>
2023-04-19 13:53:21 -0700

    mkananizadeh: Added bash scripts and environment file

---
+++ teardown_connectors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Source the .env file
+source .env
+
+# Use confluent environment
+confluent login --save
+export CCLOUD_ENV_ID=$(confluent environment list -o json \
+    | jq -r '.[] | select(.name | contains('\"${CCLOUD_ENV_NAME:-Demo_Change_Data_Capture}\"')) | .id')
+
+confluent env use $CCLOUD_ENV_ID
+
+# Use kafka cluster
+export CCLOUD_CLUSTER_ID=$(confluent kafka cluster list -o json \
+    | jq -r '.[] | select(.name | contains('\"${CCLOUD_CLUSTER_NAME:-demo_kafka_cluster}\"')) | .id')
+
+confluent kafka cluster use $CCLOUD_CLUSTER_ID
+
+# Get cluster bootstrap endpoint
+export CCLOUD_BOOTSTRAP_ENDPOINT=$(confluent kafka cluster describe -o json | jq -r .endpoint)
+
+# Get the ID for all connectors
+oracle_id=$(confluent connect cluster list -o json | jq -r '.[] | select(.name | contains ("OracleCdcSourceConnector")) | .id')
+postgres_id=$(confluent connect cluster list -o json | jq -r '.[] | select(.name | contains ("PostgresCdcSource_Products")) | .id')
+snowflake_id=$(confluent connect cluster list -o json | jq -r '.[] | select(.name | contains ("SnowflakeSinkConnector")) | .id')
+redshift_id=$(confluent connect cluster list -o json | jq -r '.[] | select(.name | contains ("RedshiftSinkConnector")) | .id')
+
+# Delete all connectors
+echo "Deleting connectors..."
+confluent connect cluster delete --force "$oracle_id"
+confluent connect cluster delete --force "$postgres_id"
+confluent connect cluster delete --force "$snowflake_id"
+confluent connect cluster delete --force "$redshift_id"
